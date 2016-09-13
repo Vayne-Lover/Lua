51ef2eac00823379e8b5dd8e28e5d70100b0448c
Vayne-Lover <406378362@qq.com>
2016-09-13 10:24:27 +0800

    Add first start project to learn Lua.

---
+++ LuaStart/main.lua
@@ -0,0 +1,27 @@
+--print("Hello World")
+--[===[32rfrf4234]===]
+--[[3476fuirhfefe]]
+--print(_VERSION)
+--[[for k,v in pairs(_G) do
+    print(k,v)
+end]]
+function foo(a,b)
+    print("foo",a)
+end
+foo(1,2)
+
+x=7
+if x==100 then
+    print(x)
+else
+    print("0")
+end
+x=2323
+if x<100 then
+    print(1)
+elseif x<200 then
+    print(2)
+else
+    print(3)
+end
+
